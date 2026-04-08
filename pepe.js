1b11a580060dfd90bd608ea14a699e4e10216f36
abamasigmaboy <obemeobunga@gmail.com>
2026-04-08 20:43:36 +0200

    pepe.js

--- pepe.js
+++ pepe.js
@@ -1,34 +1,18 @@
-//Получение Div
 let div = document.querySelector('div');
-//Получение ul
 let ul = document.querySelector('ul');
-//получение li number 2
 let li2 = document.querySelectorAll('ul')[1]
 
-
-
 let table1 = document.getElementById('age-table')
-
 table1.getElementsByTagName('label')
-
 table1.querySelector('td')[0]
-
 let form = document.getElementsByName('search')[0]
-
 form.querySelector('input')[0]
-
 let LastInput = form.lastElementChild('input') 
 form[form.Length-1]
 
-
-
-// Текст содержащийся в li
 for (let li of document.querySelectorAll('li')) {
   let title = li.firstChild.data;
-}
-//количество потомков 
+} 
 let count = li.querySelectorAll('li').Length
-
-
 
 alert(document)
